474a368618d1e1e15d90f5cd98d0491f5dcc3ef4
MiguelR2DA <miguelpriorioliveira@gmail.com>
2026-02-08 00:11:13 -0300

    TwoSum + java17

---
+++ untitled/src/EasyLevel/TwoSum.java
@@ -0,0 +1,24 @@
+package EasyLevel;
+
+public class TwoSum {
+    static void main() {
+        int[] nums = {1,10,2,5,78,8};
+        int target = 23;
+
+        twoSum(nums, target);
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
