0117c2208f8221f08dbb04e2d967b88a10d9d194
MiguelR2DA <miguelpriorioliveira@gmail.com>
2026-02-07 23:58:24 -0300

    ValidParentheses

--- untitled/src/EasyLevel/ValidParentheses.java
+++ untitled/src/EasyLevel/ValidParentheses.java
@@ -1,7 +1,33 @@
 package EasyLevel;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class ValidParentheses {
     public static void main(String[] args) {
-
+        System.out.println(isValid("{{}}"));
     }
+    public static boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        if(s.length() < 2)return false;
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '[' || c == '{' || c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+
+                char topo = stack.pop();
+
+                if(c == '}' && topo != '{') return false;
+                if(c == ']' && topo != '[') return false;
+                if(c == ')' && topo != '(') return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
 }
